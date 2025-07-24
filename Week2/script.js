09d8ccb56ab6ec1bc961b5ff87d67351dfc419c2
Unknownbosss <udeigweemmanuel0@gmail.com>
2025-07-24 17:33:26 +0100

    added icons and fixed styling

--- Week2/script.js
+++ Week2/script.js
@@ -1,5 +1,10 @@
 // wait for dom content to load
 document.addEventListener("DOMContentLoaded", () => {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth", // optional for smooth animation
+  });
+
   const display = document.getElementById("display");
   const clearButton = document.getElementById("clear");
   const history = document.getElementById("history");
@@ -181,9 +186,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   theme.addEventListener("click", () => {
     document.body.classList.toggle("dark");
-    theme.textContent = document.body.classList.contains("dark")
-      ? "light"
-      : "dark";
+    theme.innerHTML = document.body.classList.contains("dark")
+      ? `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>`
+      : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>`;
   });
   updateDisplay();
 });
